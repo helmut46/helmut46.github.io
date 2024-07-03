00fe8f99bf30706c590450feebd32123855650ae
helmut4646 <elmudfile@gmail.com>
2024-07-03 21:15:34 +0700

    add icon and fixed responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //  hamburger menu
 const hamBtn = document.querySelector('.hamburger');
 const navBtn = document.querySelector('nav ul');
+const links = document.querySelectorAll('.scroll');
 hamBtn.addEventListener('click', function(){
 	   hamBtn.classList.toggle('active');
 	   navBtn.classList.toggle('active');
@@ -16,6 +17,12 @@ window.addEventListener('scroll', () => {
     }
 });
 
+links.forEach((el) =>{
+    el.addEventListener('click', function(){
+          hamBtn.classList.remove('active');
+          navBtn.classList.remove('active');
+    });
+});
 
 
 
@@ -59,3 +66,47 @@ function eraseText() {
 window.onload = function() {
    if (totype[totypeIndex].length) setTimeout(typeText, delayTyping_text);
 } 
+
+// Smooth Scroll
+(function () {
+    const smoothScroll = function (targetEl, duration) {
+        // const headerElHeight =  document.querySelector('header').clientHeight;
+        const headerElHeight =  document.querySelector('header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+      
+        const scrollTo = function () {
+        const links = document.querySelectorAll('.scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function (e) {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+       
+ 
+
+
+    scrollTo();
+}());
